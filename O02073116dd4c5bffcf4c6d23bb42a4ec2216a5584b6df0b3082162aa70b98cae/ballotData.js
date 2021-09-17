76e29f45ea1957fbf48114a494d0c400dc2960c6
Kevin Le <kevinle108@gmail.com>
2021-09-17 17:33:10 -0400

    first commit

---
+++ O02073116dd4c5bffcf4c6d23bb42a4ec2216a5584b6df0b3082162aa70b98cae/ballotData.js
@@ -0,0 +1,174 @@
+let ballot = {
+  contests: [
+     {
+        contestName: "Question 1: Citizen Initiative",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "Q",
+        questionText: ["Do you want to ban the construction of high-impact electric transmission lines in the Upper Kennebec Region and to require the Legislature to approve all other such projects anywhere in Maine, both retroactively to 2020, and to require the Legislature, retroactively to 2014, to approve by a two-thirds vote such projects using public land?","","A \"Yes\" vote is to enact the initiated bill in its entirety.","","A \"No\" vote opposes the initiated bill in its entirety."],
+        voteFor: 1,
+        votingInstructions: "",
+        candidates: [
+           {
+              candidateName: "Yes",
+              candidateSubtitle: "",
+              candidateCode: "Yes",
+              selected: 0,
+           },
+           {
+              candidateName: "No",
+              candidateSubtitle: "",
+              candidateCode: "No",
+              selected: 0,
+           }
+        ]
+     },
+     {
+        contestName: "Question 2: Bond Issue",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "Q",
+        questionText: ["Do you favor a $100,000,000 bond issue to build or improve roads, bridges, railroads, airports, transit facilities and ports and make other transportation investments, to be used to leverage an estimated $253,000,000 in federal and other funds?","","Total estimated life time cost is $127,500,000 representing $100,000,000 in principal and $27,500,000 in interest (assuming interest at 5% over 10 years).","","A \"Yes\" vote approves the issuance of up to $100,000,000 in general obligation bonds to finance transportation-related activities.","","A \"No\" vote opposes the bond issue in its entirety."],
+        voteFor: 1,
+        votingInstructions: "",
+        candidates: [
+           {
+              candidateName: "Yes",
+              candidateSubtitle: "",
+              candidateCode: "Yes",
+              selected: 0,
+           },
+           {
+              candidateName: "No",
+              candidateSubtitle: "",
+              candidateCode: "No",
+              selected: 0,
+           }
+        ]
+     },
+     {
+        contestName: "Question 3: Constitutional Amendment",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "Q",
+        questionText: ["Do you favor amending the Constitution of Maine to declare that all individuals have a natural, inherent and unalienable right to grow, raise, harvest, produce and consume the food of their own choosing for their own nourishment, sustenance, bodily health and well-being?","","A \"Yes\" vote would amend the Maine Constitution to add a right to food.","","A \"No\" vote opposes adoption of the constitutional amendment."],
+        voteFor: 1,
+        votingInstructions: "",
+        candidates: [
+           {
+              candidateName: "Yes",
+              candidateSubtitle: "",
+              candidateCode: "Yes",
+              selected: 0,
+           },
+           {
+              candidateName: "No",
+              candidateSubtitle: "",
+              candidateCode: "No",
+              selected: 0,
+           }
+        ]
+     },
+     {
+        contestName: "CITY COUNCILOR, AT-LARGE",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "R",
+        voteFor: 1,
+        votingInstructions: "Vote for one",
+        candidates: [
+           {
+              candidateName: "Shaughnessy, Michael",
+              candidateSubtitle: "",
+              candidateCode: "1",
+              selected: 0,
+           },
+           {
+              candidateName: "",
+              candidateSubtitle: "",
+              candidateCode: "writein",
+              selected: 0,
+           }
+        ]
+     },
+     {
+        contestName: "CITY COUNCILOR, WARD 4",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "R",
+        voteFor: 1,
+        votingInstructions: "Vote for one",
+        candidates: [
+           {
+              candidateName: "Rairdon, Gary",
+              candidateSubtitle: "",
+              candidateCode: "1",
+              selected: 0,
+           },
+           {
+              candidateName: "Yurko, Kristina",
+              candidateSubtitle: "",
+              candidateCode: "2",
+              selected: 0,
+           },
+           {
+              candidateName: "",
+              candidateSubtitle: "",
+              candidateCode: "writein",
+              selected: 0,
+           }
+        ]
+     },
+     {
+        contestName: "SCHOOL COMMITTEE MEMBER, AT-LARGE",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "R",
+        voteFor: 1,
+        votingInstructions: "Vote for one",
+        candidates: [
+           {
+              candidateName: "Salisbury, Suzanne",
+              candidateSubtitle: "",
+              candidateCode: "1",
+              selected: 0,
+           },
+           {
+              candidateName: "Sardella, Tracey L.",
+              candidateSubtitle: "",
+              candidateCode: "2",
+              selected: 0,
+           },
+           {
+              candidateName: "",
+              candidateSubtitle: "",
+              candidateCode: "writein",
+              selected: 0,
+           }
+        ]
+     },
+     {
+        contestName: "QUESTION 1.",
+        contestSubtitle: "",
+        contestCode: "",
+        contestType: "Q",
+        questionText: ["Shall the City of Westbrook approve the charter amendment reprinted below?","","Section 6.2. Mayor, City Councilors and other officers; how elected, terms.","A.    Officials and terms. The municipal elections shall take place on the first Tuesday after the first Monday in November and shall be conducted in the manner provided by the laws of this state. The Mayor, two (2) City Councilors and two (2) School Committee members shall be elected from the citizens at large, by the legal voters of the City voting in their respective wards. One (1) City Councilor and one (1) School Committee member shall be elected in each ward, being residents in the ward where elected. The City Council may divide any ward into two (2) or more precincts.","[Section 6.2, A added the title \"Officials and terms\".]","","All said officers shall hold office for three (3) years from the first Monday in December, and until others shall be elected and qualified in their places.","[In this above sentence, the words \"shall be elected by a plurality of the votes given and\" were removed.]","","Wardens and Ward Clerks shall be appointed annually by the City Council based upon the recommendation of the City Clerk.","","B.     Ranked choice voting; instant runoff tabulation. For the positions of Mayor, City Councilor, and School Committee Member, the City Clerk shall implement a ranked choice voting protocol according to these guidelines:","i.	The ballot shall give voters the option of ranking candidates in order of choice.","","ii.	If a candidate receives a majority, i.e. at least one more than fifty percent (50%) of the first choice votes cast, that candidate is elected.","","iii.	If no candidate receives a majority of first choice votes, an instant runoff re-tabulation shall be promptly conducted by the City Clerk and completed within five (5) business days of the election. The instant runoff re-tabulation shall be conducted in successive rounds, with the majority determined for each successive round by the number of votes cast in that round. The candidate with the fewest votes after each successive round in which no candidate receives a majority of the votes cast in that round shall be eliminated, and the votes in the successive rounds shall be re-tabulated among the remaining candidates until one candidate receives a majority of the votes cast in that round. In each successive round, each voter's ballot shall count as a single vote for whichever candidate the voter has ranked highest who has not been eliminated in a prior round, if any.","","iv.	After the first round, a majority is determined as at least one (1) more than fifty percent (50%) of the votes cast for a remaining candidate in a particular round.","","v.	The City Clerk may adopt additional regulations consistent with this subsection to implement these provisions. The ballot shall contain instructions on how to vote for each office.","[The entirety of subsection B was added to Section 6.2.]","","Section 6.5. Vote required for election; offices to be filled; vacancies.","Whenever two (2) or more persons are to be elected to the same office, the several persons up to the number to be chosen, shall be elected by proportional representation.  The City Clerk shall implement a proportional representation voting protocol according to these guidelines:","[In this Section 6.5 wording above, \"receiving the highest number of votes shall be deemed and declared to be elected\" has been removed. The wording \"shall be elected by proportional representation. The City Clerk shall implement a proportional representation voting protocol according to these guidelines:\" were added.]","","(a)	The total number of ballots indicating a valid vote for a candidate shall be determined prior to counting of ballots for individual candidates.","(b)	Each candidate shall be credited with one vote for every valid ballot that is sorted to that candidate as first choice, or otherwise credited to that candidate as hereinafter provided, and no ballot shall ever be credited to more than one candidate at the same time.","(c)	A \"quota\" is the smallest number of votes which any candidate must receive in order to be assured of election without more candidates being elected than there are offices to be filled. It shall be determined by dividing the total number of valid ballots by one more than the total number of candidates to be elected and adding one to the result, disregarding fractions. Whenever at any stage of the counting the number of ballots credited to a candidate becomes equal to the quota, that candidate shall be declared elected, and no ballots in excess of the quota shall be credited to that candidate except as provided in (f) or (l) of this section.","(d)	The ballots shall be sorted according to the first choices marked on them.","(e)	If a candidate is elected while the ballots are being sorted according to first choices, any subsequent ballots which show that candidate as first choice shall each be credited to the second choice marked on it, or, if the second choice also has been elected, to the next choice marked on it for a candidate not yet elected.","(f)	If during the first sorting of ballots, ballots are found which are marked for a candidate already elected as first choice, but show no clear choice for any unelected candidate, such ballots shall at the end of the sorting be given to the candidate of their first choice, and in their place an equal number, as nearly as possible, of the last ballots sorted to that candidate which show a clear choice for unelected candidates, all as determined by the City Clerk, shall be taken and re-sorted to unelected candidates as if they were then being sorted for the first time.","(g)	When all the ballots have been thus sorted and credited to the first available choices marked on them, every candidate who is credited with fewer ballots than the number of signatures required for that candidate's nomination shall be declared defeated.","(h)	All the ballots of the candidates thus defeated shall be transferred, each to the candidate indicated on it as next choice among the continuing candidates. A \"continuing candidate\" is a candidate not as yet either elected or defeated. Any ballot taken for transfer which does not clearly indicate any candidate as next choice among the continuing candidates shall be set aside as \"exhausted\".","(i)	When all the ballots of the candidates thus defeated have been transferred, the one candidate who is then lowest on the poll shall be declared defeated and all that candidate's ballots transferred in the same way.","(j)	Thereupon the candidate who is then lowest shall be declared defeated and all that candidate's ballots similarly transferred; and in like manner candidates shall be declared defeated one at a time and all their ballots transferred.","(k)	If, when a candidate is to be declared defeated, two or more candidates are tied at the bottom of the poll, that one of the tied candidates shall be declared defeated who was credited with fewest ballots immediately prior to the last transfer of ballots. If two or more of the tied candidates were tied at that stage of the count, also, the second tie shall be decided by referring similarly to the standing of candidates immediately prior to the last transfer of ballots before that. This principle shall be applied successively as many times as may be necessary, a tie shown at any stage of the count being decided by referring to the standing of the tied candidates immediately prior to the last preceding transfer of ballots.  In interpreting this and other rules contained in this section the transfer of all ballots from candidates defeated together under rule (g) of this section, and the transfer of all ballots from each candidate defeated thereafter shall each constitute a single separate transfer.","(l)	Whenever candidates to the number to be elected have received the quota, any transfer of ballots in progress when the last quota was reached shall be completed, but immediately thereafter all continuing candidates shall be declared defeated and the election shall be at an end. Whenever all ballots of all defeated candidates have been transferred, and it is impossible to defeat another candidate without reducing the continuing candidates below the number still to be elected, all the continuing candidates shall be declared elected and the election shall be at an end.","(m)	A record of the count shall be kept in such form as to show, after each sorting or transfer of ballots, the number thereby credited to each candidate, the number thereby set aside as exhausted, the total for each candidate, the total set aside as exhausted, and the total number of valid ballots found by adding the totals of all candidates and the total set aside as exhausted.","(n)	Every ballot that is transferred from one candidate to another shall be stamped or marked so that its entire course from candidate to candidate can be conveniently traced.","(o)	If at any time after the first sorting of the ballots a ballot is found to have been credited to the wrong candidate, it may be transferred, as part of the transfer that is in progress, to the continuing candidate, if any, to whom it should have been credited at the time the error was made, or, if it should previously have become exhausted, may be set aside as exhausted as part of the transfer that is in progress; provided, that if the number of misplaced ballots found is sufficient to make it possible that any candidate has been wrongly defeated, so much of the sorting and transferring as may be required to correct the error shall be done over again before the count proceeds. If in correcting an error any ballots are re-sorted or re-transferred, every ballot shall be made to take the same course that it took in the original count unless the correction of an error requires its taking a different course. The principles of the rules of this section shall apply also to any recount which may be made after the original count has been completed.","[All subsections a-o of Section 6.5 were added in their entirety.]","","If it shall appear that there is no choice of Mayor, or any of the other officers to be elected from the citizens at large, or from any of the several wards, or if the person elected Mayor, or any person or persons elected to any other of the offices aforesaid, shall refuse to accept the office, or shall die before qualifying, or if a vacancy in the office of Mayor shall occur subsequently and more than three (3) months previous to the expiration of the municipal year, warrant shall forthwith be issued for a new election, and the same proceedings shall be had in all respects as hereinbefore provided, and shall be repeated until such election is completed.","","A vacancy occurring in the City Council by death, resignation or removal from the City of any member thereof, shall be filled for the unexpired term by a majority vote of the remaining members present and voting, voting by roll call; and if such vacancy be occasioned by the death, resignation or removal from the City of a member elected by a ward, the same shall be filled from the ward where it occurs.","","A vacancy occurring in the School Committee by death, resignation or removal from the city, of any member thereof, shall be filled for the unexpired term by a majority vote of the remaining members present and voting, from the ward where it occurs, or if at large from any ward.",""],
+        voteFor: 1,
+        votingInstructions: "",
+        candidates: [
+           {
+              candidateName: "Yes",
+              candidateSubtitle: "",
+              candidateCode: "Yes",
+              selected: 0,
+           },
+           {
+              candidateName: "No",
+              candidateSubtitle: "",
+              candidateCode: "No",
+              selected: 0,
+           }
+        ]
+     }
+  ]
+}
